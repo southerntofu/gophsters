382b1d4569041e646dd13aecead4273de0225cea
southerntofu <southerntofu@thunix.net>
2019-08-26 13:28:57 +0200

    Add build_template wrapper and fix raw strings for templates

--- src/templates.rs
+++ src/templates.rs
@@ -1,4 +1,7 @@
-pub const GOPHER_MAP: &str = r#"""
+use tera::{Context,Tera};
+use serde::Serialize;
+
+pub const GOPHER_MAP: &str = r#"
  .----------------.
 | .--------------. |
 | |   _____      | |
@@ -15,16 +18,16 @@ This is an unofficial Lobste.rs mirror on gopher.
 You can find the 25 hottest stories and their comments.
 Sync happens every 10 minutes or so.
 
-Last updated {{ now | date(format="%a %b %e %T %Y") }}
+Last updated {{ now() | date(format="%a %b %e %T %Y") }}
 
 {% for story in stories %}
 h[{{ story.score }}] - {{ story.title }}{% if story.url %}  URL:{{ story.url }}{% endif %}
 Submitted {{ story.date | date(format="%a %b %e %T %Y") }} by {{ story.user.name }} | {{ story.tags | join(sep=", ") }}
 0View comments ({{ story.count }}) {{ story.id }}.txt
 {% endfor %}
-"""#;
+"#;
 
-pub const GOPHER_PAGE: &str = r#"""
+pub const GOPHER_PAGE: &str = r#"
  .----------------.
 | .--------------. |
 | |   _____      | |
@@ -44,6 +47,12 @@ Viewing comments for "{{ story.title }}"
 {{ comment.user.name }} commented [{{ comment.score }}]
 {{ comment.text }}{#| cleanup(comment.indentation) }}TODO#}
 {% endfor %}
-"""#;
+"#;
 
-
+pub fn build_template<T: Serialize + ?Sized>(name: &str, context: Vec<(&str, &T)>, tera: &Tera) -> Result<String, tera::Error> {
+    let mut c = Context::new();
+    for (k, v) in context {
+        c.insert(k, v);
+    }
+    tera.render(name, &c)
+}
